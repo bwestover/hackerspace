31fbf833c640241ca02ed5bde0cbe05ffaa0a45f
bwestover <bwestover@github.com>
2018-07-21 16:34:37 -0700

    oops wrong file

--- app.js
+++ app.js
@@ -38,4 +38,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,6 @@ var router = express.Router();
 
 var db = require('../queries');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 router.get('/api/hackers', db.getAllHackers);
 router.get('/api/hackers/:id', db.getSingleHacker);
 router.post('/api/hackers', db.createHacker);
