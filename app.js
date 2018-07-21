24cb97d871a83fb523b9147891499ab9508f2e0d
bwestover <bwestover@github.com>
2018-07-21 16:44:47 -0700

    add cors package

--- app.js
+++ app.js
@@ -38,10 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+
 
 module.exports = app;
